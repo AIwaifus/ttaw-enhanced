96696c815f1725f7151ade2def67c000a5c8a29c
AIwaifus <aiwaifusoy@proton.me>
2022-05-08 09:55:45 +0000

    tmp

--- src/cmu.rs
+++ src/cmu.rs
@@ -3,4 +3,9 @@ extern crate pest;
 use error::Error;
 use metaphone::{Rule, Word};
 use pest::Parser;
-use
+use std::collections::HashMap;
+use std::fs;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+pub st
