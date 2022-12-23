aca99f9357eaeedc92c405c6ec99bfa5f20250fb
AIwaifus <aiwaifusoy@proton.me>
2022-12-23 19:17:52 +0000

    Add Procfile.

--- src/cmu.rs
+++ src/cmu.rs
@@ -173,4 +173,9 @@ fn from_json_file(path: &Path) -> Result<HashMap<String, Vec<Vec<String>>>, Erro
 
 pub fn download_and_serialize(path: &Path) -> Result<(), Error> {
     let dict_string = reqwest::blocking::get(
-        "https://raw.githubusercontent.com/cmusp
+        "https://raw.githubusercontent.com/cmusphinx/cmudict/master/cmudict.dict",
+    )?
+    .text()?;
+
+    let cursor = io::Cursor::new(dict_string);
+    let lines = cursor.lines().c
