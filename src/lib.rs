ba6baf91cd3b632fb06a7e15b2845ef4835eca25
AIwaifus <aiwaifusoy@proton.me>
2023-01-31 13:10:20 +0000

    Remove generated files

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,8 @@ extern crate pest;
 /// ```rust
 /// extern crate ttaw;
 /// use ttaw;
-/// let cmudict = ttaw::cmu::CmuDict:
+/// let cmudict = ttaw::cmu::CmuDict::new("cmudict.json").unwrap();
+/// assert_eq!(
+///     cmudict.encoding("permeability"),
+///     Ok(Some(vec![vec![
+///         "P".to_stri
