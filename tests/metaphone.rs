7f2c8870b45863c9fe6f88e6f6e46e461cffa430
AIwaifus <aiwaifusoy@proton.me>
2023-02-24 21:54:10 +0000

    Bumps version number

--- tests/metaphone.rs
+++ tests/metaphone.rs
@@ -75,3 +75,119 @@ fn no_rhyme() {
 
     assert!(!rhyme("empty", "  "));
     assert!(!rhyme("empty", ""));
+    assert!(!rhyme("empty", "\t"));
+    assert!(!rhyme("empty", "\r"));
+    assert!(!rhyme("empty", "\n"));
+}
+
+#[test]
+fn ptah() {
+    assert_eq!(
+        encoding("ptah"),
+        DoubleMetaphone {
+            primary: "PT".to_string(),
+            secondary: "PT".to_string()
+        }
+    )
+}
+
+#[test]
+fn ceasar() {
+    assert_eq!(
+        encoding("ceasar"),
+        DoubleMetaphone {
+            primary: "SSR".to_string(),
+            secondary: "SSR".to_string()
+        }
+    )
+}
+
+#[test]
+fn ach() {
+    assert_eq!(
+        encoding("ach"),
+        DoubleMetaphone {
+            primary: "AK".to_string(),
+            secondary: "AK".to_string()
+        }
+    )
+}
+
+#[test]
+fn chemical() {
+    assert_eq!(
+        encoding("chemical"),
+        DoubleMetaphone {
+            primary: "KMKL".to_string(),
+            secondary: "KMKL".to_string()
+        }
+    )
+}
+
+#[test]
+fn choral() {
+    assert_eq!(
+        encoding("choral"),
+        DoubleMetaphone {
+            primary: "KRL".to_string(),
+            secondary: "KRL".to_string()
+        }
+    )
+}
+
+#[test]
+fn aleksander() {
+    assert_eq!(encoding("alexander"), encoding("aleksander"))
+}
+
+#[test]
+fn hiccups() {
+    assert_eq!(encoding("HICCUPS"), encoding("HiCcUpS"));
+    assert_eq!(encoding("HiCcUpS"), encoding("hiccups"));
+}
+
+#[test]
+fn gnarl() {
+    assert_eq!(encoding("gnarl").primary.get(..1), Some("N"));
+}
+
+#[test]
+fn knack() {
+    assert_eq!(encoding("knack").primary.get(..1), Some("N"));
+}
+
+#[test]
+fn pneumatic() {
+    assert_eq!(encoding("pneumatic").primary.get(..1), Some("N"));
+}
+
+#[test]
+fn wrack() {
+    assert_eq!(encoding("wrack").primary.get(..1), Some("R"));
+}
+
+#[test]
+fn psycho() {
+    assert_eq!(encoding("psycho").primary.get(..1), Some("S"));
+}
+
+#[test]
+fn xavier() {
+    assert_eq!(encoding("Xavier").primary.get(..1), Some("S"));
+}
+
+#[test]
+fn vowels() {
+    assert_eq!(encoding("a").primary, "A");
+    assert_eq!(encoding("e").primary, "A");
+    assert_eq!(encoding("i").primary, "A");
+    assert_eq!(encoding("o").primary, "A");
+    assert_eq!(encoding("u").primary, "A");
+    assert_eq!(encoding("y").primary, "A");
+}
+
+#[test]
+fn drop_initial() {
+    assert_eq!(encoding("ba").primary.len(), 1);
+    assert_eq!(encoding("be").primary.len(), 1);
+    assert_eq!(encoding("bi").primary.len(), 1);
